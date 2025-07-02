9f1291d56a25995d8a9c31e30f75f30b0d4ac773
markbogorad <mabogorad@gmail.com>
2025-07-02 09:18:22 -0400

    secret adjsuted

--- rcm_secrets.py
+++ rcm_secrets.py
@@ -1,8 +1,5 @@
-import toml
+import streamlit as st
 
-with open("secrets.toml", "r") as f:
-    secrets = toml.load(f)
-
-MAPBOX_TOKEN = secrets["api_keys"]["mapbox"]
-SERPAPI_KEY = secrets["api_keys"]["serpapi"]
-MAX_RESULTS = secrets["api_keys"].get("max_results", 50)
+MAPBOX_TOKEN = st.secrets["api_keys"]["mapbox"]
+SERPAPI_KEY = st.secrets["api_keys"]["serpapi"]
+MAX_RESULTS = st.secrets["api_keys"].get("max_results", 50)
